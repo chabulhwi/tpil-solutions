7c9bc4634c44098241cb2de9ec05b9509042ff65
Bulhwi Cha <chabulhwi@semmalgil.com>
2025-02-04 11:47:58 +0900

    chore: update `TPIL.lean`
    I forgot to import `TPIL.Chapter04.Quantifier` in the `TPIL.lean` file.

--- TPIL.lean
+++ TPIL.lean
@@ -5,3 +5,4 @@ import TPIL.Chapter02.«Question19-20»
 import TPIL.Chapter02.«Question22-25»
 import TPIL.Chapter02.«Question34-38»
 import TPIL.Chapter04.PredicateAndRelation
+import TPIL.Chapter04.Quantifier
