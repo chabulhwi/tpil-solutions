cf871d824fd42697da31974b276cad230b79a5be
Bulhwi Cha <chabulhwi@semmalgil.com>
2025-09-14 12:21:18 +0900

    doc: add copyright info and doc headers

--- TPIL/Chapter05/Exercise02.lean
+++ TPIL/Chapter05/Exercise02.lean
@@ -1,3 +1,15 @@
+/-
+Copyright (c) 2025 Bulhwi Cha. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Bulhwi Cha
+-/
+
+/-!
+# Chapter 5 Exercises
+
+## Exercise 2
+-/
+
 example (p q r : Prop) (hp : p)
         : (p ∨ q ∨ r) ∧ (q ∨ p ∨ r) ∧ (q ∨ r ∨ p) := by
   repeat (first | apply And.intro | left; assumption | right | assumption)
